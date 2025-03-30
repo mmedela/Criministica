86ce7533007ac35814b9d48cf7ac949c5cdf19bc
mmedela <65471451+mmedela@users.noreply.github.com>
2025-03-30 18:13:29 -0300

    fix estadisticas schema

--- schemas/estadistica_schema.py
+++ schemas/estadistica_schema.py
@@ -18,9 +18,19 @@ class EstadisticaUpdate(EstadisticaBase):
     pass
 
 class EstadisticaResponse(EstadisticaBase):
-    estadistica_id: int
+    id: int
     provincia_id: int
     codigo_delito_snic_id: int
+    anio: int
+    cantidad_hechos: Optional[int] = 0
+    cantidad_victimas: Optional[int] = 0
+    cantidad_victimas_masc: Optional[int] = 0
+    cantidad_victimas_fem: Optional[int] = 0
+    cantidad_victimas_sd: Optional[int] = 0
+    tasa_hechos: Optional[float] = 0.0
+    tasa_victimas: Optional[float] = 0.0
+    tasa_victimas_masc: Optional[float] = 0.0
+    tasa_victimas_fem: Optional[float] = 0.0
     
     class Config:
         from_attributes = True
