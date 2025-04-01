904fed4b7dd3384914b21efe3f31f5df6124f0af
mmedela <65471451+mmedela@users.noreply.github.com>
2025-04-01 19:17:22 -0300

    refactor schema for new column

--- schemas/provincia_schema.py
+++ schemas/provincia_schema.py
@@ -1,16 +1,18 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class ProvinciaBase(BaseModel):
     provincia_nombre: str
 
 class ProvinciaCreate(ProvinciaBase):
-    pass
+    poblacion: Optional[int] = None
 
-class ProvinciaUpdate(ProvinciaBase):
-    pass
+class ProvinciaUpdate(BaseModel):
+    poblacion: Optional[int] = None
 
 class ProvinciaResponse(ProvinciaBase):
     provincia_id: int
+    poblacion: Optional[int] = None
     
     class Config:
         from_attributes = True
