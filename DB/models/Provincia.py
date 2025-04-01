03cc4144e939652401108840224ccaa51fc6b171
mmedela <65471451+mmedela@users.noreply.github.com>
2025-04-01 19:16:54 -0300

    feat: add new column to provincia table

--- DB/models/Provincia.py
+++ DB/models/Provincia.py
@@ -5,9 +5,10 @@ class Provincia(Base):
 
     provincia_id = Column(Integer, primary_key=True)
     provincia_nombre = Column(String)
-    
+    poblacion = Column(Integer, nullable=True)
+
     # Relación con estadísticas de delitos
     estadisticas = relationship('EstadisticaDelito', back_populates='provincia')
-    
+
     def __repr__(self):
-        return f"<Provincia(id={self.provincia_id}, nombre={self.provincia_nombre})>"
+        return f"<Provincia(id={self.provincia_id}, nombre={self.provincia_nombre}, poblacion={self.poblacion})>"
